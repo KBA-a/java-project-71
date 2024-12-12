e0cc9f18f8b26361268c295f8992a564c4487175
KBA-a <mackh2121@gmail.com>
2024-12-12 22:47:44 +0500

    add option "-h and -v"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     application
 }
+ // required
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
@@ -15,8 +16,11 @@ application {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+    description = "Compares two configuration files and shows a difference.")
+
 public class App {
+    /*@Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private File file;
+
+    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+    private String algorithm = "SHA-256";*/
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
