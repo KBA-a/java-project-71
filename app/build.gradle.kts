c75012cb07b030f91c744b8f4674d1ca012165be
KBA-a <mackh2121@gmail.com>
2024-12-14 23:55:08 +0500

    add read and parsing json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,12 @@ application {
 dependencies {
     implementation("info.picocli:picocli:4.7.6")
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
+    implementation ("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
+    implementation ("com.fasterxml.jackson.core:jackson-core:2.17.2")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.1")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,18 +5,17 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
-import java.math.BigInteger;
+
 import java.nio.file.Files;
-import java.security.MessageDigest;
-import java.util.concurrent.Callable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import com.fasterxml.jackson.databind.*;
+import java.util.Map;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
     description = "Compares two configuration files and shows a difference.")
 
 public class App {
-    /*@Parameters(index = "0", description = "The file whose checksum to calculate.")
-    private File file;*/
 
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format;
@@ -27,8 +26,32 @@ public class App {
     @Parameters(paramLabel = "filepath2", description = "path to second file")
     String filepath2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        String readFilePathFirst = "src/main/java/hexlet/code/fixtures/file1.json";
+        String readFilePathSecond = "src/main/java/hexlet/code/fixtures/file2.json";
+        ObjectMapper mapper = new ObjectMapper();
+
+        Path filePathFirst = Paths.get(readFilePathFirst).toAbsolutePath().normalize();
+        Path filePathSecond = Paths.get(readFilePathSecond).toAbsolutePath().normalize();
+
+        if (!Files.exists(filePathFirst)) {
+            throw new Exception("File:" + filePathFirst + " - does not exist!");
+        }
+        if (!Files.exists(filePathSecond)) {
+            throw new Exception("File:" + filePathSecond + " - does not exist!");
+        }
+
+        String textInFirstFile = Files.readString(filePathFirst);
+        String textInFirstSecond = Files.readString(filePathSecond);
+
+        Map<String, Object> parsFirstFile = mapper.readValue(textInFirstFile, Map.class);
+        Map<String, Object> parsSecondFile = mapper.readValue(textInFirstSecond, Map.class);
+
+
         System.out.println("Hello world!");
+        System.out.println(parsFirstFile);
+        System.out.println(parsSecondFile);
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
